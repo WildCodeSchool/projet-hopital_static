799d00eab7c8048684fd3c7e8ae39443c34b2404
Florian Grandjean <florian.pdf@gmail.com>
2016-02-29 12:11:16 +0100

    test

--- wp-content/themes/weavex-child/header.php
+++ wp-content/themes/weavex-child/header.php
@@ -33,6 +33,11 @@ if (function_exists('weaverx_ts_pp_switch'))	// switching to alternate theme?
 
 <!-- Latest compiled and minified JavaScript -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+
+<!-- Jquery -->
+<script src="//code.jquery.com/jquery-1.12.0.min.js"></script>
+<script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <?php
 	$viewport = "<meta name='viewport' content='width=device-width,initial-scale=1.0' />\n"; /* use full horizontal size on iPad */
